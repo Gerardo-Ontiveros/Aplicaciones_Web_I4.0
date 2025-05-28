49a6185ec45098c8d681e2ec8221109eb1028568
Gerardo-Ontiveros <gontiverosdavila01@gamail.com>
2025-05-27 22:15:04 -0600

    Add connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import express from "express"
-import morgan from "morgan"
-import authRoutes from "./routes/auth.routes"
+import express from "express";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.routes";
+import connectDBMongo from "./config/db";
 
 const app = express(); //CREANDO SERVIDOR
 const PORT = 3000; //NUMERO DE PUERTO
 
+console.log("El servidor se esta alojando en el puerto 3000");
+app.use(express.json()); //REQUEST EN TIPO JSON
+app.use(morgan("dev"));
 
-console.log("El servidor se esta alojando en el puerto 3000")
-app.use(express.json()) //REQUEST EN TIPO JSON
-app.use(morgan("dev"))
+app.use("/api/auth", authRoutes);
 
-app.use('/api/auth', authRoutes)
-
-
-app.listen(PORT, ()=>{
-    console.log(`EL SERVIDOR ESTA EN EL PUERTO ${PORT}`)
-})
+connectDBMongo().then(() => {
+  app.listen(PORT, () => {
+    console.log(`EL SERVIDOR ESTA EN EL PUERTO ${PORT}`);
+  });
+});
