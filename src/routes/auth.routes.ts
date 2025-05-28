854ac6dea243eff3a9c08a67eb888bc0eec9c21a
Gerardo-Ontiveros <gontiverosdavila01@gamail.com>
2025-05-27 22:17:12 -0600

    add routes for the controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
-import { login } from "../controllers/auth.controller";
+import {
+  getAllUsers,
+  getUserByUsername,
+  login,
+  saveUser,
+} from "../controllers/auth.controller";
+import { getTimeToken } from "../controllers/auth.controller";
+import { updateToken } from "../controllers/auth.controller";
 
-const router=Router();
+const router = Router();
 
 /*
 Utiliza el endpoint login por medio 
 de la ruta /login-user y metodo post
 */
 
-router.post('/login', login);
+router.post("/login", login);
+router.get("/getTime/:id", getTimeToken);
+router.patch("/update/:userId", updateToken);
+router.get("/users", getAllUsers);
+router.post("/users/add", saveUser);
+router.get("/users/name/:userName", getUserByUsername);
 
-
-export default router
+export default router;
